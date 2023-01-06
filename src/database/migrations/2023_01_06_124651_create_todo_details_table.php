e3e02511cc300b458c383046dda6ee752d0e9851
SantaCruz77 <steveyamada66@gmail.com>
2023-01-06 22:19:15 +0900

    set database

--- src/database/migrations/2023_01_06_124651_create_todo_details_table.php
+++ src/database/migrations/2023_01_06_124651_create_todo_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateTodoDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('todo_details', function (Blueprint $table) {
+            $table->id();
+            $table -> string('detail');
+            $table->timestamps();
+            $table->foreignId('todo_id')->constrained();
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('todo_details');
     }
 }
